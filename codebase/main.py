210465d897f177fe700d27cb03cdde914b6cbe33
rishabh shukla <33500470+rysabh@users.noreply.github.com>
2023-12-18 18:04:58 +0530

    Update main.py
    color detection

--- codebase/main.py
+++ codebase/main.py
@@ -1,11 +1,39 @@
 import cv2 as cv
-import os
-import sys
+import numpy as np
+# import os
+# import sys
 
 
 # print(os.getcwd())
+image = cv.imread('assets/test_color.png')
 
-with open('assets/test_color.png') as file:
-    cv.imshow(file)
+# def sample_layer(image):
+#     # red = image[:,:,0]
 
+def detect_color(image):
+    # Convert the image to HSV color space
+    # image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+    # Define the range for the color red
+    lower_red = np.array([0, 0, 0])
+    upper_red = np.array([200, 200, 255])
+
+
+    # Create a mask for the color red
+    mask = cv.inRange(image, lower_red, upper_red)
+
+   # Display the mask
+    # cv.imshow('Mask', mask)
+    # cv.waitKey(0)
+    # Bitwise-AND mask and original image
+    res = cv.bitwise_and(image, image, mask=mask)
+
+    return res
+
+# print(image.shape)
+
+newImage = detect_color(image)
+
+cv.imshow('Test Image', newImage)
+cv.waitKey(0)
 
